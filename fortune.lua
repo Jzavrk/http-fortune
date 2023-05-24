94e00571dedb219e57b88d1ba7ace6e75464d3f2
Jzavrk <cynek@gmx.com>
2023-05-24 21:26:30 +0200

    Bulk editor

--- fortune.lua
+++ fortune.lua
@@ -2,47 +2,45 @@
 ---@author Jzavrk
 
 local M = {
-	fortunes = {},
+	fortune_list = {},
 	printable_list = "",
 }
 
 --- List possible categories
-function M.setup()
-	local fortunes = assert(io.popen("fortune -f 2>&1 1> /dev/null"), "fortune is missing")
-	-- Discard first line (path to fortunes)
-	local _ = fortunes:read("l")
-	for it in fortunes:lines("l") do
-		local categorie = string.match(it, "[-_%a]+")
-		M.fortunes[categorie] = true
-		M.printable_list = M.printable_list .. categorie .. "\n"
+function M.init()
+	local categories = assert(io.popen("fortune -f 2>&1 1> /dev/null"), "fortune is missing")
+	-- Discard first line (path to fortune_list)
+	local _ = categories:read("l")
+	for it in categories:lines("l") do
+		local category = string.match(it, "[-_%a]+")
+		table.insert(M.fortune_list, category)
+		M.fortune_list[category] = true
+		M.printable_list = M.printable_list .. category .. "\n"
 	end
-	io.close(fortunes)
+	io.close(categories)
 end
 
---- Get fortune by categorie
----@param categorie string
----@return string|nil,string|nil categorie name and fortune body
-function M.get_fortune(categorie)
-	if not M.fortunes[categorie] then
-		return nil, nil
-	end
-	local handler = assert(io.popen("fortune -a " .. categorie), "fortune is missing")
+--- Get fortune by category
+---@param category string
+---@return string|nil,string|nil category name and fortune body
+function M.get_fortune(category)
+	local handler = assert(io.popen("fortune -a " .. category), "fortune is missing")
 	local fortune = handler:read("a")
 	io.close(handler)
 
-	return categorie, fortune
+	return category, fortune
 end
 
 --- Get random fortune
----@return string,string Categorie and fortune body
+---@return string,string category and fortune body
 function M.get_random_fortune()
 	local handler = assert(io.popen("fortune -ac"), "fortune is missing")
-	local categorie = handler:read("l"):match("[-_%a]+")
+	local category = handler:read("l"):match("[-_%a]+")
 	local _ = handler:read("l")
 	local fortune = handler:read("a")
 	io.close(handler)
 
-	return categorie, fortune
+	return category, fortune
 end
 
 return M
--- server.lua
+++ server.lua
@@ -10,10 +10,19 @@ local http_headers = require "http.headers"
 local http_tls = require "http.tls"
 local fortune = require "fortune"
 
-local function reply(myserver, stream) -- luacheck: ignore 212
+local help_msg = [[Fortune Teller
+
+List categories: /categories
+Generate by category: /category (lower-case)
+
+example: https://getfortune.click/linux
+]]
+
+local function reply(app, stream) -- luacheck: ignore 212
 	-- Read in headers
 	local req_headers = assert(stream:get_headers())
 	local req_method = req_headers:get ":method"
+	local req_path = req_headers:get ":path"
 
 	-- Log request to stdout
 	assert(io.stdout:write(string.format('[%s] "%s %s HTTP/%g"  "%s" "%s"\n',
@@ -27,42 +36,55 @@ local function reply(myserver, stream) -- luacheck: ignore 212
 
 	-- Build response headers
 	local res_headers = http_headers.new()
+	local res_body = ""
 	res_headers:append(":status", "200")
-	res_headers:append("content-type", "text/plain")
-	-- Send headers to client; end the stream immediately if this was a HEAD request
-	assert(stream:write_headers(res_headers, req_method == "HEAD"))
-	if req_method ~= "HEAD" then
-		-- Send body, ending the stream
-		local fortune_cookie = select(2, fortune.get_random_fortune())
-		assert(stream:write_chunk(fortune_cookie, true))
+	res_headers:append("content-type", "text/plain; charset=utf-8")
+	if req_method == "GET" then
+		if req_path == "/" then
+			local fortune_head, fortune_body = fortune.get_random_fortune()
+			res_body = string.format("%s\n\n%s", fortune_head:gsub("^%l", string.upper), fortune_body)
+		elseif req_path == "/categories" then
+			res_body = fortune.printable_list
+		elseif req_path == "/help" then
+			res_body = help_msg
+		else	-- "/*"
+			local category = string.sub(req_path, 2)
+			if fortune.fortune_list[category] then
+				res_body = select(2, fortune.get_fortune(category)) or ""
+			else
+				res_headers:upsert(":status", "404")
+				res_body = "Not found"
+			end
+		end
+		-- Send headers to client; end the stream immediately if this was a HEAD request
+		assert(stream:write_headers(res_headers, req_method == "HEAD"))
+		assert(stream:write_body_from_string(res_body))
 	end
 end
 
-fortune.setup()
+fortune.init()
 local custom_context = http_tls.new_server_context()
-local myserver = assert(http_server.listen {
-	-- host = "0.0.0.0",
-	path = '/srv/http/fortune.socket',
+local app = assert(http_server.listen({
+	path = "/srv/http/fortune.socket",
 	ctx = custom_context,
 	unlink = true,
-	mode = "0750",
-	-- mask = "0750",
+	mode = "0770",
 	onstream = reply,
-	onerror = function(myserver, context, op, err, errno) -- luacheck: ignore 212
+	onerror = function(app, context, op, err, errno) -- luacheck: ignore 212
 		local msg = op .. " on " .. tostring(context) .. " failed"
 		if err then
 			msg = msg .. ": " .. tostring(err)
 		end
 		assert(io.stderr:write(msg, "\n"))
 	end,
-})
+}))
 
 -- Manually call :listen() so that we are bound before calling :localname()
-assert(myserver:listen())
+assert(app:listen())
 do
-	local path = select(2, myserver:localname())
+	local path = select(2, app:localname())
 	assert(io.stderr:write(string.format("Listening on socket %s\n", path)))
 end
 -- Start the main server loop
-assert(myserver:loop())
--- myserver:close()
+assert(app:loop())
+-- app:close()
